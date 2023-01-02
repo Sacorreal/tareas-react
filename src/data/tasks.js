0cf0cf0c0156d80b79c6ef52a1e295773d8dd47b
Schneider <schneidercorrea@gmail.com>
2023-01-01 21:12:41 -0500

    primera versión de la aplicación

---
+++ src/data/tasks.js
@@ -0,0 +1,5 @@
+export const tasks = [
+  { id: 0, title: "Mi primer tarea", description: "tengo que estudiar React" },
+  { id: 1, title: "Mi segunda tarea", description: "Curso de POO en Platzi" },
+  { id: 2, title: "Mi tercera tarea", description: "Curso de NextJS con Bluuweb" },
+];
